f7c9cea2fb8bc4398fc52acc339814e0f2c88e26
Karthick Dharman <karthick.dharman@bootlabstech.com>
2022-01-09 20:53:05 +0530

    <No ID> <terraform template standardisation>

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "folder_name" {
+  description = "the display name of the folder"
+  type        = string
+}
+
+variable "parent_resource_name" {
+  description = "the resource name of the parent folder or organization"
+  type        = string
+}
