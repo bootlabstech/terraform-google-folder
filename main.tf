f08daab39a8c25815427eea6729f661c2396823f
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-16 18:05:38 +0530

    rem tag

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_folder" "folder" {
   display_name = var.folder_name
   parent       = var.parent_resource_name
     lifecycle {
-    ignore_changes = [tags,labels]
+    ignore_changes = [labels]
   }
 }
